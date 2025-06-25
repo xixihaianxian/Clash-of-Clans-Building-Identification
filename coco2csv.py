185ae82d01e40cd737bfc1743ae9561b78bc59bf
xixihaianxian <1668584651@qq.com>
2025-06-25 23:02:53 +0800

    2025/6/25 23:02

--- coco2csv.py
+++ coco2csv.py
@@ -7,18 +7,15 @@ def get_coco_file_path(data_dir):
     coco_file = os.path.join(data_dir,'_annotations.coco.json')
     return coco_file
 
-def coco2csv(coco_file, coco_dir, csv_file):
+def coco2csv(coco_file, csv_file):
 
     coco = COCO(coco_file)
 
     image_ids = coco.getImgIds()
     images = coco.loadImgs(image_ids)
-    print(images)
     print(f"寻找到图片{len(images)}张！")
 
     all_annotations = list() #TODO 所有annotations
-    all_heights= list()
-    all_widths= list()
     
     for image in images:
         ann_ids = coco.getAnnIds(imgIds=image['id'], iscrowd=None)
@@ -30,23 +27,24 @@ def coco2csv(coco_file, coco_dir, csv_file):
     print(f"寻找到annotations{len(annotations)}个！")
 
     annotations[['x1', 'y1', 'x2', 'y2']] = pd.DataFrame(annotations['bbox'].values.tolist(), index=annotations.index).astype(int)
-    annotations = annotations.assign(x2 = annotations['x1'] + annotations['x2'], y2 = annotations['y1'] + annotations['y2'])
+    annotations = annotations.assign(x2 = annotations['x1'] + annotations['x2'], y2 = annotations['y1'] + annotations['y2']) 
+    #TODO 将x1,y1,height,width转化x1,y1x2,y2
 
-    # images = pd.DataFrame(images)
-    # images.rename(columns={'id':'image_id'}, inplace=True)
-    # images.set_index('image_id')
+    images = pd.DataFrame(images)
+    images.rename(columns={'id':'image_id'}, inplace=True)
+    images.set_index('image_id')
 
-    # annotations_csv = pd.merge(annotations, images, on=['image_id'], how='right')
-    # annotations_csv = annotations_csv.replace(np.nan, '', regex=True)
+    annotations_csv = pd.merge(annotations, images, on=['image_id'], how='right')
+    annotations_csv = annotations_csv.replace(np.nan, '', regex=True)
 
-    # colnames = ['file_name', 'x1','y1','x2','y2']
-    # annotations_csv = annotations_csv[colnames]
+    colnames = ["file_name","height","width","x1","y1","x2","y2","category_id"]
+    annotations_csv = annotations_csv[colnames]
 
-    # annotations_csv.to_csv(path_or_buf=csv_file, index=False, header=False, columns=colnames)
+    annotations_csv.to_csv(path_or_buf=csv_file, index=False, header=True, columns=colnames)
     
 if __name__=="__main__":
     
     coco_dir="train"
     csv_file=os.path.join(coco_dir,f"{coco_dir}.csv")
     coco_file=get_coco_file_path(data_dir=coco_dir)
-    coco2csv(coco_file=coco_file,coco_dir="train",csv_file=csv_file)
+    coco2csv(coco_file=coco_file,csv_file=csv_file)
